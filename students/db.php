0cf0dcbfa7a08669f6146b05c1de28569104e41e
Zac <a19860101@gmail.com>
2022-03-12 16:58:27 +0800

    20220312

--- students/db.php
+++ students/db.php
@@ -1,12 +1,26 @@
 <?php
+    // $db_host = 'localhost';
+    // $db_user = 'admin';
+    // $db_pw = 'admin';
+    // $db_name = 'php-6-20220305';
+
+    // $db = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫連線錯誤，請洽詢資料庫管理員');
+
+    // // print_r($db);
+
+    // //在php中執行sql語法
+    // mysqli_query($db,'SET NAMES utf8mb4');
+    // ---------------------------------------------------------------------//
+
+
     $db_host = 'localhost';
     $db_user = 'admin';
     $db_pw = 'admin';
     $db_name = 'php-6-20220305';
 
-    $db = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫連線錯誤，請洽詢資料庫管理員');
+    $db = new mysqli($db_host,$db_user,$db_pw,$db_name);
+    if($db->connect_errno){
+        echo $db->connect_error;
+    }
 
-    // print_r($db);
-
-    //在php中執行sql語法
-    mysqli_query($db,'SET NAMES utf8mb4');
+    $db->query('SET NAMES utf8mb4');
