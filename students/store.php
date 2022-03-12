54ff31bd277a027a6418140d9e5373cac439b267
Zac <a19860101@gmail.com>
2022-03-12 14:53:43 +0800

    20220312

--- students/store.php
+++ students/store.php
@@ -5,3 +5,7 @@
     $sql = "INSERT INTO students(name,email,edu,gender,skill,content,created_at,updated_at)VALUES('$name','$email','$edu','$gender','$skill','$content',now(),now())";
     
     mysqli_query($db,$sql);
+
+    // header('location:index.php');
+    echo "<script>alert('學生資料已建立!');</script>";
+    header('refresh:0;url=index.php');
