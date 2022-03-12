1886c22dfcc992fe95763320c3edb84bd33d5069
Zac <a19860101@gmail.com>
2022-03-12 12:05:15 +0800

    20220312

---
+++ students/db.php
@@ -0,0 +1,9 @@
+<?php
+    $db_host = 'localhost';
+    $db_user = 'admin';
+    $db_pw = 'admin';
+    $db_name = 'php-6-20220305';
+
+    $db = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('資料庫連線錯誤，請洽詢資料庫管理員');
+
+    // print_r($db);
