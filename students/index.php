7637269611d429c55d9c7bfaa7e0e31c75c508fc
Zac <a19860101@gmail.com>
2022-03-12 14:23:02 +0800

    20220312

--- students/index.php
+++ students/index.php
@@ -16,16 +16,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table,td,th {
+            border-collapse: collapse;
+            border: 1px solid #999;
+            padding: 10px;
+        }
+        
+    </style>
 </head>
 <body>
-    <?php
-        foreach($datas as $data){
-            echo '<div>'.$data['name'].'</div>';
-            echo '<div>'.$data['email'].'</div>';
-            echo '<div>'.$data['gender'].'</div>';
-            echo '<div>'.$data['skill'].'</div>';
-            echo '<hr>';
-        }
-    ?>
+    <table>
+        <tr>
+            <th>序號</th>
+            <th>姓名</th>
+            <th>Email</th>
+            <th>性別</th>
+            <th>學歷</th>
+            <th>專長</th>
+            <th>備註</th>
+            <th>建立時間</th>
+        </tr>
+
+    <?php foreach($datas as $data){ ?>
+        <tr>
+            <td><?php echo $data['id']; ?></td>
+            <td><?php echo $data['name']; ?></td>
+            <td><?php echo $data['email']; ?></td>
+            <td><?php echo $data['gender']; ?></td>
+            <td><?php echo $data['edu']; ?></td>
+            <td><?php echo $data['skill']; ?></td>
+            <td><?php echo $data['content']; ?></td>
+            <td><?php echo $data['created_at']; ?></td>
+        </tr>
+    <?php } ?>
+    </table>
 </body>
 </html>
