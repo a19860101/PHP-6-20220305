aeda328ec38c3e9e1392c8a69719c29240ec0570
Zac <a19860101@gmail.com>
2022-03-19 14:57:34 +0800

    20220319

---
+++ filesystem/upload.php
@@ -0,0 +1,17 @@
+<?php
+    extract($_FILES['img']);
+    // echo $_FILES['img']['name'];
+    echo $name;
+    echo '<br>';
+    // echo $_FILES['img']['size'];
+    echo $size;
+    echo '<br>';
+    // echo $_FILES['img']['error'];
+    echo $error;
+    echo '<br>';
+    // echo $_FILES['img']['tmp_name'];
+    echo $tmp_name;
+    echo '<br>';
+    // echo $_FILES['img']['type'];
+    echo $type;
+    echo '<br>';
