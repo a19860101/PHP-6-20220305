a5ba5a2c4292151c7660ff50dea4fa11ec499a34
Zac <a19860101@gmail.com>
2022-03-19 10:57:49 +0800

    20220319

--- students_pdo/db.php
+++ students_pdo/db.php
@@ -8,5 +8,31 @@
     $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
     // data source name
 
-    $pdo = new PDO($dsn,$db_user,$db_pw);
+    try {
+        $pdo = new PDO($dsn,$db_user,$db_pw);
 
+        //不主動報錯(預設值)
+        // $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);
+        
+        //主動報錯
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+        
+        //主動例外
+        // $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+  
+
+    try {
+        $sql = 'SELECT * FROM sudents';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $data = $stmt->fetch();
+    
+        print_r($data);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
