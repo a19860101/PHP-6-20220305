2ca2fd70cc333162526fdbaee62bc37245525a94
Zac <a19860101@gmail.com>
2022-04-09 14:07:29 +0800

    20220409

--- member/template/nav.php
+++ member/template/nav.php
@@ -1,11 +1,21 @@
+<?php 
+    if(!session_id()){
+        session_start();
+    }
+?>
+<nav>
+    <div class="logo">
+        <a href="index.php">LOGO</a>
+    </div>
+    <div>
+        <?php if(isset($_SESSION['AUTH'])){ ?>
+            <a href="#"><?php echo $_SESSION['AUTH']['name'];?></a>
+            <a href="logout.php">登出</a>
+        <?php } ?>
 
-    <nav>
-        <div class="logo">
-            <a href="index.php">LOGO</a>
-        </div>
-        <div>
+        <?php if(!isset($_SESSION['AUTH'])){ ?>
             <a href="register.php">註冊會員</a>
             <a href="login.php">登入</a>
-            <a href="#">登出</a>
-        </div>
-    </nav>
+        <?php } ?>
+    </div>
+</nav>
