ed2a24d2778294fbe02b08cf3eddf31d44d86f27
Zac <a19860101@gmail.com>
2022-04-09 11:35:23 +0800

    20220409

--- member/register.php
+++ member/register.php
@@ -5,12 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
-<?php include('template/nav.php'); ?>
-<h1>註冊會員</h1>
-
+    <?php include('template/nav.php'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h2>註冊會員</h2>
+                <form action="">
+                    <div class="mb-3">
+                        <label for="" class="form-label">帳號</label>
+                        <input type="text" name="user" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">密碼</label>
+                        <input type="password" name="pw" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">E mail</label>
+                        <input type="text" name="email" class="form-control">
+                    </div>
+                    <input type="submit" value="註冊" class="btn btn-primary">
+                    <input type="button" value="取消" class="btn btn-outline-danger">
+                </form>
+            </div>
+        </div>
+    </div>
+    
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
