007077f9764a6166a31843ed3d036ce0ade6442e
Zac <a19860101@gmail.com>
2022-04-09 15:24:17 +0800

    20220409

--- class/basic2.php
+++ class/basic2.php
@@ -3,11 +3,20 @@
         public $x = 100;
         private $y = 50;
         //只可使用在自己的類別
+
+        protected $z = 99;
+
+        function qwer(){
+            return $this->z;
+        }
     }
 
     class Test2 extends Test {
-
+        function asdf(){
+            return $this->z;
+        }
     }
 
     $john = new Test2;
-    echo $john->y;
+    // echo $john->z;
+    echo $john->asdf();
