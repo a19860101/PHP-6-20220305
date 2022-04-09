3c83111252cbae9a4520bbda348c7467b96dc596
Zac <a19860101@gmail.com>
2022-04-09 11:02:54 +0800

    20220409

---
+++ member/template/nav.php
@@ -0,0 +1,11 @@
+
+    <nav>
+        <div class="logo">
+            <a href="index.php">LOGO</a>
+        </div>
+        <div>
+            <a href="register.php">註冊會員</a>
+            <a href="login.php">登入</a>
+            <a href="#">登出</a>
+        </div>
+    </nav>
