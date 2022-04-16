df66d37c5c66829eb3d70e611811bb16bc91d5f8
Zac <a19860101@gmail.com>
2022-04-16 14:54:19 +0800

    20220416

--- blog/public/post/delete.php
+++ blog/public/post/delete.php
@@ -1,6 +1,12 @@
 <?php
     include('../../vendor/autoload.php');
     use Gjun\Blog\Controller\Post;
+    use Gjun\Blog\Controller\File;
+
+    if($_REQUEST['cover'] != null){
+        $path = 'images/'.$_REQUEST['cover'];
+        File::delete($path);
+    }
     Post::delete($_REQUEST);
 
     header('location:../index.php');
--- blog/public/post/show.php
+++ blog/public/post/show.php
@@ -6,7 +6,14 @@
 <?php include('../template/header.php'); ?>
 <div class="container">
     <div class="row">
-        <div class="col-6">
+        <div class="col-12">
+            <div class="mb-3" style="height:400px;">
+                <?php if($post['cover'] != null){ ?>
+                <img src="images/<?php echo $post['cover'];?>" class="w-100 h-100" style="object-fit:cover;">
+                <?php }else{ ?>
+                <img src="https://picsum.photos/id/123/800" class="w-100 h-100"  style="object-fit:cover;">
+                <?php } ?>
+            </div>
             <h2 class="mb-3"><?php echo $post['title']; ?></h2>
             <div>
                 <?php echo $post['content']; ?>
@@ -16,6 +23,7 @@
                 <a href="edit.php?id=<?php echo $post['id'];?>" class="btn btn-success">編輯</a>
                 <form action="delete.php" method="post" class="d-inline-block">
                     <input type="hidden" value="<?php echo $post['id']; ?>" name="id">
+                    <input type="hidden" value="<?php echo $post['cover']; ?>" name="cover">
                     <input type="submit" class="btn btn-danger" value="刪除" onclick="return confirm('確認刪除？')">
                 </form>
             </div>
