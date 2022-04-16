a808eda83cc1b87e80c7c8f98a1f72099bf660f8
Zac <a19860101@gmail.com>
2022-04-16 10:56:45 +0800

    20220416

--- blog/public/index.php
+++ blog/public/index.php
@@ -15,7 +15,7 @@
             <div class="border p-3 rounded-3">
                 <h3 class="mb-3"><?php echo $post['title'];?></h3>
                 <div>
-                    <?php echo $post['content']; ?>
+                    <?php echo mb_substr($post['content'],0,100); ?>...
                 </div>
                 <div class="my-3">
                     <a href="post/show.php?id=<?php echo $post['id'];?>" class="btn btn-primary btn-sm">繼續閱讀</a>
