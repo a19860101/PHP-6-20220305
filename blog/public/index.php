88d06670bae36190548c51c065834342858ff636
Zac <a19860101@gmail.com>
2022-04-16 09:38:41 +0800

    20220416

--- blog/public/index.php
+++ blog/public/index.php
@@ -1,6 +1,13 @@
 <?php
     include('../vendor/autoload.php');
+?>
+<?php include('template/header.php'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>title</h1>
+        </div>
+    </div>
+</div>
+<?php include('template/footer.php'); ?>
 
-    use Gjun\Blog\Controller\Student;
-
-    print_r(Student::index());
