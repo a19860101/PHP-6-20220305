076d7a04db288c37313c5ca493d5fea9d2ccf276
Zac <a19860101@gmail.com>
2022-04-23 14:28:18 +0800

    laravel

---
+++ laravel/resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>about</h1>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut commodi ab error autem doloribus, voluptates id repellendus veritatis harum nulla aspernatur qui temporibus sunt! Accusantium recusandae necessitatibus odit eligendi voluptate?
+    </p>
+</body>
+</html>
