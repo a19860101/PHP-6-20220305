d002f2a2d82c89b5dcc7cdb2479be926f4859609
Zac <a19860101@gmail.com>
2022-04-23 14:04:28 +0800

    20220423

--- blog/public/post/gallery.php
+++ blog/public/post/gallery.php
@@ -1,6 +1,10 @@
 <?php
     $imgs = glob('images/*');
-    var_dump($imgs);
+
+    if(isset($_REQUEST['del'])){
+        unlink($_REQUEST['img']);
+        header('location:gallery.php');
+    }
 ?>
 <?php include('../template/header.php'); ?>
 <div class="container">
@@ -9,8 +13,9 @@
         <div class="col-3">
             <div>
                 <img src="<?php echo $img; ?>" class="w-100">
-                <form action="">
-                    <input type="submit" class="btn btn-danger btn-sm" value="刪除">
+                <form action="" method="post">
+                    <input type="hidden" name="img" value="<?php echo $img; ?>">
+                    <input type="submit" class="btn btn-danger btn-sm" value="刪除" name="del">
                 </form>
             </div>
         </div>
