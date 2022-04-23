a5c7332687d58a42f65039c6b3923cb7e9688ae8
Zac <a19860101@gmail.com>
2022-04-23 10:22:47 +0800

    2022.0423

--- blog/public/category/index.php
+++ blog/public/category/index.php
@@ -9,8 +9,22 @@
     <div class="row">
         <div class="col-12">
             <h2>分類管理</h2>
+            <hr>
         </div>
-        <div class="col-9"></div>
+        <div class="col-9">
+            <h3>新增分類</h3>
+            <form action="store.php" method="post">
+                <div class="mb-3">
+                    <label for="" class="form-label">分類標題</label>
+                    <input type="text" name="title" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">分類英文標題</label>
+                    <input type="text" name="slug" class="form-control">
+                </div>
+                <input type="submit" value="新增分類" class="btn btn-primary">
+            </form>
+        </div>
         <div class="col-3">
             <h3>所有分類</h3>
             <ul class="list-group">
