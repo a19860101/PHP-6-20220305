32e850b043f3e0d2db93a7549e9c98f767f50d5f
Zac <a19860101@gmail.com>
2022-04-30 12:09:00 +0800

    20220430

--- laravel/resources/views/post/index.blade.php
+++ laravel/resources/views/post/index.blade.php
@@ -3,17 +3,33 @@
 - 首頁
 @endsection
 @section('main')
-<h1>文章列表</h1>
-@foreach($posts as $post)
-<div>
-    <h2>{{$post->title}}</h2>
-    <small>{{$post->created_at}}</small>
-    <div>
-        {{$post->content}}
-    </div>
-    <div>
-        <a href="{{route('post.show',['post'=>$post->id])}}">繼續閱讀</a>
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            @foreach($posts as $post)
+            <div>
+                <div>
+                    <img src="https://picsum.photos/id/11/800/400" alt="">
+                </div>
+                <div>
+                    <h3>{{$post->title}}</h3>
+                    <div>{{$post->content}}</div>
+                    <a href="{{route('post.show',['post'=>$post->id])}}">繼續閱讀</a>
+                </div>
+                <div>
+                    {{$post->created_at}} JohnDoe
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <div class="col-4">
+            <div>
+                <h2>分類</h2>
+            </div>
+            <div>
+                <h2>最新文章</h2>
+            </div>
+        </div>
     </div>
 </div>
-@endforeach
 @endsection
--- laravel/resources/views/template/nav.blade.php
+++ laravel/resources/views/template/nav.blade.php
@@ -1,21 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">首頁</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('post.index')}}">所有文章</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('post.create')}}">新增文章</a>
-          </li>
-        </ul>
-      </div>
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">首頁</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('post.index') }}">所有文章</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('post.create') }}">新增文章</a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </nav>
+</nav>
