61730c8b66328ceefb79fbb4fb30261ca8f868b8
Zac <a19860101@gmail.com>
2022-04-30 12:30:02 +0800

    20220430

--- laravel/resources/views/post/index.blade.php
+++ laravel/resources/views/post/index.blade.php
@@ -5,17 +5,17 @@
 @section('main')
 <div class="container">
     <div class="row">
-        <div class="col-8">
+        <div class="col-9">
             @foreach($posts as $post)
-            <div class="row align-items-center mb-3">
+            <div class="row mb-3">
                 <div class="col-6">
                     <img src="https://picsum.photos/id/11/800/600" class="w-100">
                 </div>
-                <div class="col-6">
+                <div class="col-6 d-flex flex-column justify-content-between">
                     <div>
                         <h3>{{$post->title}}</h3>
                         <div>{{$post->content}}</div>
-                        <a href="{{route('post.show',['post'=>$post->id])}}">繼續閱讀</a>
+                        <a href="{{route('post.show',['post'=>$post->id])}}" class="btn btn-outline-primary btn-sm">繼續閱讀</a>
                     </div>
                     <div>
                         {{$post->created_at}} JohnDoe
@@ -24,7 +24,7 @@
             </div>
             @endforeach
         </div>
-        <div class="col-4">
+        <div class="col-3">
             <div>
                 <h2>分類</h2>
             </div>
