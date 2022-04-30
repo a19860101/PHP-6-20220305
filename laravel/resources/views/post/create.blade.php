fe7d00086e2a3b7ff1564b9b171076232b2b0650
Zac <a19860101@gmail.com>
2022-04-30 14:22:49 +0800

    20220430

--- laravel/resources/views/post/create.blade.php
+++ laravel/resources/views/post/create.blade.php
@@ -3,17 +3,23 @@
     - 新增文章
 @endsection
 @section('main')
-<h1>新增文章</h1>
-<form action="/post" method="post">
-    @csrf
-    <div>
-        <label for="">標題</label>
-        <input type="text" name='title'>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h2>新增文章</h2>
+            <form action="/post" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="" class="form-label">標題</label>
+                    <input type="text" name='title' class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">內文</label>
+                    <textarea name="content" id="" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <input type="submit" value="新增文章" class="btn btn-primary">
+            </form>
+        </div>
     </div>
-    <div>
-        <label for="">內文</label>
-        <textarea name="content" id="" cols="30" rows="10"></textarea>
-    </div>
-    <input type="submit" value="新增文章">
-</form>
+</div>
 @endsection
--- laravel/resources/views/post/edit.blade.php
+++ laravel/resources/views/post/edit.blade.php
@@ -3,18 +3,25 @@
     - 編輯文章
 @endsection
 @section('main')
-    <h1>編輯文章</h1>
-    <form action="{{route('post.update',['post'=>$post->id])}}" method="post">
-        @csrf
-        @method('put')
-        <div>
-            <label for="">標題</label>
-            <input type="text" name='title' value="{{$post->title}}">
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h2>編輯文章</h2>
+            <form action="{{route('post.update',['post'=>$post->id])}}" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="" class="form-label">標題</label>
+                    <input type="text" name='title' class="form-control" value="{{$post->title}}">
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">內文</label>
+                    <textarea name="content" id="" cols="30" rows="10" class="form-control">{{$post->content}}</textarea>
+                </div>
+                <input type="submit" value="儲存" class="btn btn-primary">
+                <input type="button" value="取消" onclick="history.back()" class="btn btn-outline-danger">
+            </form>
         </div>
-        <div>
-            <label for="">內文</label>
-            <textarea name="content" id="" cols="30" rows="10">{{$post->content}}</textarea>
-        </div>
-        <input type="submit" value="儲存">
-        <input type="button" value="取消" onclick="history.back()">
+    </div>
+</div>
+
 @endsection
