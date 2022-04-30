32ad9f73ab8e88eef8148a32c5b59efc88a890c0
Zac <a19860101@gmail.com>
2022-04-30 11:58:18 +0800

    20220430

--- laravel/resources/views/template/nav.blade.php
+++ laravel/resources/views/template/nav.blade.php
@@ -1,5 +1,21 @@
-<nav>
-    <a href="{{route('post.index')}}">文章列表</a>
-    <a href="{{route('post.create')}}">新增文章</a>
-    <a href="#">tests</a>
-</nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">首頁</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('post.index')}}">所有文章</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('post.create')}}">新增文章</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
