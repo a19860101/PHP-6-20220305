942a3c7cde22655e9aefa4dd5e08cb8555b55df1
Zac <a19860101@gmail.com>
2022-05-14 14:23:06 +0800

    20220514

--- laravel/app/Product.php
+++ laravel/app/Product.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes;
     //
     protected $fillable = ['title','desc','price','sale','started_at','ended_at'];
 }
